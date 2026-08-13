36d4afeb5a3705640c37095255ed908f48704a2e
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2026-08-13 11:00:33 -0400

    Compose darwin ghostty config

--- home-manager/darwin-modules/ghostty.nix
+++ home-manager/darwin-modules/ghostty.nix
@@ -1,33 +1,11 @@
 { pkgs, ... }:
 
 {
+  imports = [ ../modules/ghostty.nix ];
+
   programs.ghostty = {
-    enable = true;
     package = pkgs.ghostty-bin;
-    enableFishIntegration = true;
-    installBatSyntax = true;
-    clearDefaultKeybinds = true;
     settings = {
-      quit-after-last-window-closed = true;
-      confirm-close-surface = false;
-
-      font-feature = [
-        "ss01"
-        "ss02"
-        "ss03"
-        "ss04"
-        "ss05"
-        "ss06"
-        "ss07"
-        "ss08"
-        "ss09"
-        "calt"
-        "liga"
-      ];
-
-      window-padding-x = 8;
-      window-padding-y = 8;
-
       macos-titlebar-style = "hidden";
       macos-dock-drop-behavior = "window";
 
@@ -35,17 +13,6 @@
       macos-icon-frame = "plastic";
       macos-icon-ghost-color = "black";
       macos-icon-screen-color = "black";
-
-      window-inherit-working-directory = false;
-
-      keybind = [
-        "super+ctrl+r=reload_config"
-        "super+==increase_font_size:1"
-        "super++=increase_font_size:1"
-        "super+-=decrease_font_size:1"
-        "paste=paste_from_clipboard"
-        "super+v=paste_from_clipboard"
-      ];
     };
   };
 }
