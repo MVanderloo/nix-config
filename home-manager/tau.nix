e75e52d212f38b3ad796cfae0ed28b5640c5351d
Michael van der Loo <me@mvanderloo.com>
2026-08-24 18:08:30 -0500

    disable ghostty module on tau

--- home-manager/tau.nix
+++ home-manager/tau.nix
@@ -7,7 +7,7 @@
   imports = [
     ./modules/editor.nix
     ./modules/fish.nix
-    ./modules/ghostty.nix
+    # ./modules/ghostty.nix
     ./modules/ssh-gpg.nix
     ./modules/tmux.nix
     ./modules/version-control.nix
