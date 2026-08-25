49eb1d31831374e8518bd0320de887b6d029263f
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2026-08-25 12:02:47 -0400

    update env vars to be strings

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -5,8 +5,8 @@
       autoUpdate = true;
       upgrade = false;
       extraEnv = {
-        HOMEBREW_NO_ENV_HISTS = 1;
-        HOMEBREW_NO_ANALYTICS = 1;
+        HOMEBREW_NO_ENV_HISTS = "1";
+        HOMEBREW_NO_ANALYTICS = "1";
       };
     };
     enableFishIntegration = true;
