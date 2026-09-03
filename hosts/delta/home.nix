f965b4d0e1e2cc2046c23d81dd37820f305bdf82
Michael van der Loo <me@mvanderloo.com>
2026-09-02 20:34:42 -0400

    nvidia support

--- hosts/delta/home.nix
+++ hosts/delta/home.nix
@@ -59,4 +59,18 @@
   # };
 
   services.podman.enable = true;
+
+  nixpkgs.config = {
+    allowUnfreePackages = [ "nvidia-x11" ];
+    nvidia.acceptLicense = true;
+  };
+
+  targets.genericLinux.gpu = {
+    enable = true;
+    nvidia = {
+      enable = true;
+      version = "610.57.04";
+      sha256 = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
+    };
+  };
 }
