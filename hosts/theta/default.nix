283591f03160889be4105de0158532156168e899
Michael van der Loo <me@mvanderloo.com>
2026-09-05 21:18:47 -0400

    disable waylandcraft-desktop

--- hosts/theta/default.nix
+++ hosts/theta/default.nix
@@ -6,7 +6,7 @@
     inputs.home-manager.nixosModules.home-manager
     inputs.preservation.nixosModules.preservation
     inputs.sops-nix.nixosModules.sops
-    inputs.waylandcraft-desktop.nixosModules.default
+    # inputs.waylandcraft-desktop.nixosModules.default
 
     ../../modules/nix-settings.nix
     ../../modules/nixos/bash.nix
@@ -21,7 +21,7 @@
     ./hardware-configuration.nix
     ./preservation.nix
     # ./services/trmnl-node-lite.nix
-    ./waylandcraft.nix
+    # ./waylandcraft.nix
   ];
 
   home-manager = {
