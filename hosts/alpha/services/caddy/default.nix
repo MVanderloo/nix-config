b7f6232072edab5b50dadad02c21cb2c3391c5ae
Michael van der Loo <me@mvanderloo.com>
2026-09-06 14:09:57 -0400

    use native caddy

--- hosts/alpha/services/caddy/default.nix
+++ hosts/alpha/services/caddy/default.nix
@@ -1,8 +1,5 @@
 { config, ... }:
 
-let
-  stateDirectory = "/var/lib/caddy";
-in
 {
   assertions = [
     {
@@ -11,70 +8,22 @@ in
     }
   ];
 
-  networking.firewall = {
-    allowedTCPPorts = [
-      80
-      443
-    ];
-    allowedUDPPorts = [ 443 ];
+  services.caddy = {
+    enable = true;
+    configFile = ./Caddyfile;
+    openFirewall = true;
   };
 
-  virtualisation.oci-containers.containers.caddy = {
-    image = "docker.io/library/caddy:2.11.4-alpine@sha256:5f5c8640aae01df9654968d946d8f1a56c497f1dd5c5cda4cf95ab7c14d58648";
-
-    capabilities = {
-      ALL = false;
-      NET_BIND_SERVICE = true;
-    };
-
-    extraOptions = [
-      "--health-cmd=curl --fail --silent --show-error http://127.0.0.1:2019/config/"
-      "--health-interval=30s"
-      "--health-on-failure=kill"
-      "--health-retries=3"
-      "--health-start-period=5s"
-      "--health-timeout=5s"
-      "--memory=256m"
-      "--memory-swap=256m"
-      "--pids-limit=128"
-      "--read-only"
-      "--security-opt=no-new-privileges"
-      "--tmpfs=/tmp:rw,nosuid,nodev,noexec,size=16m"
-    ];
-
-    podman.sdnotify = "healthy";
-
-    ports = [
-      "80:80/tcp"
-      "443:443/tcp"
-      "443:443/udp"
-    ];
-
-    pull = "missing";
-
-    volumes = [
-      "${./Caddyfile}:/etc/caddy/Caddyfile:ro"
-      "${stateDirectory}/config:/config"
-      "${stateDirectory}/data:/data"
-    ];
-  };
-
-  systemd = {
-    services.podman-caddy = {
-      requires = [ "tailscaled.service" ];
-      after = [ "tailscaled.service" ];
-    };
-
-    tmpfiles.rules = [
-      "d ${stateDirectory} 0700 root root -"
-      "d ${stateDirectory}/config 0700 root root -"
-      "d ${stateDirectory}/data 0700 root root -"
-    ];
+  systemd.services.caddy = {
+    requires = [ "tailscaled.service" ];
+    after = [ "tailscaled.service" ];
   };
 
   preservation.preserveAt."/persist".directories = [
     {
-      directory = stateDirectory;
+      directory = "/var/lib/caddy";
+      user = "caddy";
+      group = "caddy";
       mode = "0700";
     }
   ];
