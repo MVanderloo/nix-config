d078fd8a125f7599ea1fd1b2b663ece1f6517c5e
Michael van der Loo <me@mvanderloo.com>
2026-09-09 01:02:44 -0400

    add password prompt to plymouth penguin

--- modules/nixos/penguin-plymouth.nix
+++ modules/nixos/penguin-plymouth.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
+  cfg = config.boot.plymouth.penguin;
   themeName = "penguin";
 
   themeScript = pkgs.writeText "${themeName}.script" ''
@@ -27,6 +33,18 @@ let
     animation.sprite.SetX(screen.w / 2 - frame.image[0].GetWidth() / 2);
     animation.sprite.SetY(screen.h / 2 - frame.image[0].GetHeight() / 2);
 
+    dialog.image = Image("password-box.png");
+    dialog.sprite = Sprite(dialog.image);
+    dialog.x = screen.w / 2 - dialog.image.GetWidth() / 2;
+    dialog.y = screen.h / 2 + frame.image[0].GetHeight() / 2 + 32;
+
+    if (dialog.y + dialog.image.GetHeight() > screen.h - 24)
+      dialog.y = screen.h - dialog.image.GetHeight() - 24;
+
+    dialog.sprite.SetX(dialog.x);
+    dialog.sprite.SetY(dialog.y);
+    dialog.sprite.SetOpacity(0);
+
     fun refresh() {
       animation.sprite.SetImage(frame.image[Math.Int(frame.position) % frame.count]);
       frame.position += frame.speed;
@@ -35,29 +53,55 @@ let
     Plymouth.SetRefreshFunction(refresh);
 
     fun display_password(prompt_text, bullet_count) {
-      prompt.image = Image.Text(prompt_text, 1, 1, 1);
+      if (prompt.sprite)
+        prompt.sprite.SetOpacity(0);
+      if (bullets.sprite)
+        bullets.sprite.SetOpacity(0);
+
+      dialog.sprite.SetOpacity(1);
+
+      prompt.image = Image.Text(
+        "Unlock NixOS",
+        1, 1, 1, 1,
+        "Sans Bold ${toString cfg.promptFontSize}",
+        "center"
+      );
       prompt.sprite = Sprite(prompt.image);
       prompt.sprite.SetX(screen.w / 2 - prompt.image.GetWidth() / 2);
-      prompt.y = screen.h / 2 + frame.image[0].GetHeight() / 2 + 24;
+      prompt.y = dialog.y + 22;
       prompt.sprite.SetY(prompt.y);
 
-      bullets.text = "";
+      bullets.text = "Password: ";
       for (i = 0; i < bullet_count; i++)
         bullets.text += "*";
 
-      bullets.image = Image.Text(bullets.text, 1, 1, 1);
+      bullets.image = Image.Text(
+        bullets.text,
+        0.8, 0.82, 0.9, 1,
+        "Monospace ${toString cfg.promptFontSize}",
+        "center"
+      );
       bullets.sprite = Sprite(bullets.image);
       bullets.sprite.SetX(screen.w / 2 - bullets.image.GetWidth() / 2);
-      bullets.sprite.SetY(prompt.y + prompt.image.GetHeight() + 12);
+      bullets.sprite.SetY(prompt.y + prompt.image.GetHeight() + 18);
     }
 
     Plymouth.SetDisplayPasswordFunction(display_password);
 
     fun display_question(prompt_text, answer_text) {
-      question.image = Image.Text(prompt_text + " " + answer_text, 1, 1, 1);
+      if (question.sprite)
+        question.sprite.SetOpacity(0);
+
+      dialog.sprite.SetOpacity(1);
+      question.image = Image.Text(
+        prompt_text + " " + answer_text,
+        1, 1, 1, 1,
+        "Sans Bold ${toString cfg.promptFontSize}",
+        "center"
+      );
       question.sprite = Sprite(question.image);
       question.sprite.SetX(screen.w / 2 - question.image.GetWidth() / 2);
-      question.sprite.SetY(screen.h / 2 + frame.image[0].GetHeight() / 2 + 24);
+      question.sprite.SetY(dialog.y + dialog.image.GetHeight() / 2 - question.image.GetHeight() / 2);
     }
 
     Plymouth.SetDisplayQuestionFunction(display_question);
@@ -72,10 +116,15 @@ let
     Plymouth.SetMessageFunction(display_message);
 
     fun display_normal() {
-      prompt.sprite = null;
-      bullets.sprite = null;
-      question.sprite = null;
-      message.sprite = null;
+      dialog.sprite.SetOpacity(0);
+      if (prompt.sprite)
+        prompt.sprite.SetOpacity(0);
+      if (bullets.sprite)
+        bullets.sprite.SetOpacity(0);
+      if (question.sprite)
+        question.sprite.SetOpacity(0);
+      if (message.sprite)
+        message.sprite.SetOpacity(0);
     }
 
     Plymouth.SetDisplayNormalFunction(display_normal);
@@ -90,7 +139,20 @@ let
         themeDir="$out/share/plymouth/themes/${themeName}"
 
         mkdir -p "$themeDir"
-        magick ${../../assets/penguin.gif} -coalesce "$themeDir/frame-%03d.png"
+        magick ${../../assets/penguin.gif} \
+          -coalesce \
+          -resize ${toString cfg.imageSize}x${toString cfg.imageSize} \
+          "$themeDir/frame-%03d.png"
+        magick \
+          -size ${toString cfg.dialogWidth}x${toString cfg.dialogHeight} \
+          xc:'#181825' \
+          -fill none \
+          -stroke '#cdd6f4' \
+          -strokewidth 3 \
+          -draw 'roundrectangle 2,2,${toString (cfg.dialogWidth - 3)},${
+            toString (cfg.dialogHeight - 3)
+          },18,18' \
+          "$themeDir/password-box.png"
         cp ${themeScript} "$themeDir/${themeName}.script"
 
         cat > "$themeDir/${themeName}.plymouth" <<EOF
@@ -106,7 +168,33 @@ let
       '';
 in
 {
-  boot = {
+  options.boot.plymouth.penguin = {
+    imageSize = lib.mkOption {
+      type = lib.types.ints.positive;
+      default = 320;
+      description = "Width and height of the penguin animation in pixels.";
+    };
+
+    dialogWidth = lib.mkOption {
+      type = lib.types.ints.positive;
+      default = 560;
+      description = "Width of the visible password dialog in pixels.";
+    };
+
+    dialogHeight = lib.mkOption {
+      type = lib.types.ints.positive;
+      default = 120;
+      description = "Height of the visible password dialog in pixels.";
+    };
+
+    promptFontSize = lib.mkOption {
+      type = lib.types.ints.positive;
+      default = 18;
+      description = "Font size used in the password dialog.";
+    };
+  };
+
+  config.boot = {
     plymouth = {
       enable = true;
       theme = themeName;
