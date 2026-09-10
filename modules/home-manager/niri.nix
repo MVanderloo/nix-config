c0835cdb2644a8f2327491f8a9ecc56e2c14bbab
Michael van der Loo <me@mvanderloo.com>
2026-09-10 16:36:56 -0400

    update helium binary name

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -26,7 +26,7 @@
         "Mod+Space".spawn-sh = "noctalia msg panel-toggle launcher";
         "Mod+D".spawn-sh = "noctalia msg panel-toggle launcher";
         "Mod+Ctrl+Q".spawn-sh = "noctalia msg session lock";
-        "Mod+B".spawn-sh = "helium";
+        "Mod+B".spawn-sh = "helium-browser";
         "MOD+E".spawn-sh = "nautilus";
 
         "XF86AudioRaiseVolume" = {
